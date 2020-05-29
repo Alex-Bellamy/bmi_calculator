7bf833c25dc3777c920fbf930d8c0b0019fe83d2
Alex Bellamy <alexlbellamy@gmail.com>
2020-05-29 18:41:10 +0200

    creating assets folder and files and writing second test

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,5 +1,6 @@
 describe('User can check their bmi', () => {
     it('User can enter weight ad height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
     })
 })
