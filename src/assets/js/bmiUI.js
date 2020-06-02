41a0c5d679096726486951d26aee7bddb797359b
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-02 09:16:43 +0200

    Adding BMI classifications and amending output message to user

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,7 +1,8 @@
 const  calculateBMI = () => {
 	let  weightInput = document.getElementById("weight")
     let  heightInput = document.getElementById("height")
-    let  results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let  result = calculateMetricBMI(weightInput.value, heightInput.value)
+    let  message = getBMIClassification(result)
     let  displayResultsElement = document.getElementById('results')
-	displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
+	displayResultsElement.innerHTML = `<h1> Your BMI value is: ${result} and you are ${message}</h1>`
 }
